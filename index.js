ce1f6039bf1a305a40183ddff78ffe0416971eac
Kamil Mirza <kamilmirza250611250611@gmail.com>
2025-09-20 13:16:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,441 @@
+
+let opn1 = document.querySelector(".opn1");
+let opn2 = document.querySelector(".opn2");
+let google = document.getElementById("google");
+let zoracard = document.querySelector(".zoracard");
+let listning = document.getElementById("listening");
+let textcont = document.getElementById("textcont");
+document.getElementById("menu").addEventListener("click",()=>{
+  let sc = document.querySelector(".social");
+  sc.style.display="flex";
+});
+document.getElementById("md1").addEventListener("click",()=>{
+  let sc = document.querySelector(".social");
+  sc.style.display="none";
+});
+document.getElementById("md2").addEventListener("click",()=>{
+  window.open("https://www.blackbox.ai/","_blank")
+});
+document.getElementById("md3").addEventListener("click",()=>{
+  window.open("https://gemini.google.com/app?hl=en-IN","_blank")
+});
+document.getElementById("md4").addEventListener("click",()=>{
+  window.open("https://copilot.microsoft.com/","_blank")
+});
+document.getElementById("md5").addEventListener("click",()=>{
+  window.open("https://www.microsoft.com/en-in/","_blank")
+});
+document.getElementById("md6").addEventListener("click",()=>{
+  window.open("https://www.amazon.in/","_blank")
+});
+document.getElementById("md7").addEventListener("click",()=>{
+  window.open("https://www.flipkart.com/","_blank")
+});
+document.getElementById("md8").addEventListener("click",()=>{
+  window.open("https://www.facebook.com/","_blank")
+});
+document.getElementById("md9").addEventListener("click",()=>{
+  window.open("https://web.whatsapp.com/","_blank")
+});
+document.getElementById("md10").addEventListener("click",()=>{
+  window.open("https://www.instagram.com/","_blank")
+});
+document.getElementById("md11").addEventListener("click",()=>{
+  window.open("https://newjaisa.com/?srsltid=AfmBOordYyrLs5hu7XAGj7plu2MW_Qx8Kequmf-Gd_d7zskiWek7S7pb","_blank")
+});
+document.getElementById("md12").addEventListener("click",()=>{
+  window.open("https://www.olx.in/en-in","_blank")
+});
+document.getElementById("md13").addEventListener("click",()=>{
+  window.open("https://www.autodesk.com/in/products?mktvar002=5019031|SEM|22515084165|185938565704|kwd-15326526&utm_source=GGL&utm_medium=SEM&utm_campaign=GGL_DEC_Autodesk_APAC_IN_eComm_SEM_BR_New_EX_0000_5019031_PureBrand&utm_id=5019031&utm_term=kwd-15326526&mkwid=s|pcrid|750614064948|pkw|autodesk|pmt|e|pdv|c|slid||pgrid|185938565704|ptaid|kwd-15326526|pid|&utm_medium=cpc&utm_source=google&utm_campaign=&utm_term=autodesk&utm_content=s|pcrid|750614064948|pkw|autodesk|pmt|e|pdv|c|slid||pgrid|185938565704|ptaid|kwd-15326526|&gad_source=1&gad_campaignid=22515084165&gbraid=0AAAAADMqutmSp4EkF63EhLPhJBG14DC87&gclid=Cj0KCQjw_rPGBhCbARIsABjq9cd-YbD0eRXddpIL2Bej41CY5SAojBu9sYQCRLzooV2pXhMnc84zhuYaAraaEALw_wcB","_blank")
+});
+document.getElementById("md14").addEventListener("click",()=>{
+  window.open("https://tallysolutions.com/tally-prime/?srsltid=AfmBOop-HsMJMkl9cTTYG5j4YBLIQfbqz2FG5M3YYGDtSszOH5kTZjWJ","_blank")
+});
+document.getElementById("md15").addEventListener("click",()=>{
+  window.open("https://www.kinemaster.com/","_blank")
+});
+document.getElementById("md16").addEventListener("click",()=>{
+  window.open("https://www.tinkercad.com/","_blank")
+});
+document.getElementById("md17").addEventListener("click",()=>{
+  window.open("https://www.hp.com/in-en/shop/?intel_sem&jumpid=ps_in_go_st_mc_cm018701_cv_se_bcr&utm_source=google&utm_medium=cpc&utm_campaign=IN_B-Core_SEM_Mix_all_Acq_Google-s_IC_ENG_CM018701_INTEL-CCF-Cons-matched_EM_AllDev_incbn_Always-ON-N%26E&gclsrc=aw.ds&gad_source=1&gad_campaignid=22949173843&gbraid=0AAAAACY0IB1RszfG6Po-TfBRm4wmbg5ge&gclid=Cj0KCQjw_rPGBhCbARIsABjq9ccBiIX4N0FHOqyPP7YMt_aayK_A2ULlyo0ljNDoNGp8Yy4Kk1uwtfYaAiGPEALw_wcB","_blank")
+});
+document.getElementById("md18").addEventListener("click",()=>{
+  window.open("https://www.dell.com/en-in","_blank")
+});
+document.getElementById("md19").addEventListener("click",()=>{
+  window.open("https://www.lenovo.com/us/en/pc/?orgRef=https%253A%252F%252Fwww.google.com%252F&srsltid=AfmBOop9XuLJcjvNw37SRiPuyiTeRjhBKqXGjuZR0tvOTXGmHV9wcKAg","_blank")
+});
+document.getElementById("md20").addEventListener("click",()=>{
+  window.open("https://www.asus.com/","_blank")
+});
+document.getElementById("md21").addEventListener("click",()=>{
+  window.open("https://www.youtube.com/","_blank")
+});
+document.getElementById("md22").addEventListener("click",()=>{
+  window.open("https://x.com/?lang=en-in","_blank")
+});
+document.getElementById("md23").addEventListener("click",()=>{
+  window.open("https://web.telegram.org/k/","_blank")
+});
+document.getElementById("md24").addEventListener("click",()=>{
+  window.open("https://www.snapchat.com/","_blank")
+});
+document.getElementById("md25").addEventListener("click",()=>{
+  window.open("https://www.tor-2.org/","_blank")
+});
+document.getElementById("md26").addEventListener("click",()=>{
+  window.open("https://dotnet.microsoft.com/en-us/download","_blank")
+});
+document.getElementById("md27").addEventListener("click",()=>{
+  window.open("https://www.netflix.com/in/login","_blank")
+});
+document.getElementById("md28").addEventListener("click",()=>{
+  window.open("https://sarkariresult.com.cm/","_blank")
+  window.open("https://www.sarkariresult.com/","_blank")
+  window.open("https://sarkarialert.net/","_blank")
+  window.open("https://www.sarkariresult.app/","_blank")
+});
+document.getElementById("md29").addEventListener("click",()=>{
+  window.open("https://www.nielit.gov.in/","_blank")
+});
+document.getElementById("md30").addEventListener("click",()=>{
+  window.open("https://chatgpt.com/","_blank")
+});
+document.getElementById("md31").addEventListener("click",()=>{
+  window.open("https://www.oracle.com/","_blank")
+});
+document.getElementById("md32").addEventListener("click",()=>{
+  window.open("https://sourceforge.net/projects/mingw-w64/","_blank")
+});
+document.getElementById("md33").addEventListener("click",()=>{
+  window.open("https://www.python.org/downloads/","_blank")
+});
+document.getElementById("md34").addEventListener("click",()=>{
+  window.open("https://nodejs.org/en/download/","_blank")
+});
+document.getElementById("md35").addEventListener("click",()=>{
+  window.open("https://getbootstrap.com/","_blank")
+});
+document.getElementById("md36").addEventListener("click",()=>{
+  window.open("https://github.com/","_blank")
+});
+document.getElementById("md37").addEventListener("click",()=>{
+  window.open("https://www.mongodb.com/","_blank")
+});
+document.getElementById("md38").addEventListener("click",()=>{
+  window.open("https://www.kali.org/","_blank")
+});
+document.getElementById("md39").addEventListener("click",()=>{
+  window.open("https://www.coderabbit.ai/","_blank")
+});
+document.getElementById("md40").addEventListener("click",()=>{
+  window.open("https://www.deepseek.com/en","_blank")
+});
+document.getElementById("nop4").addEventListener("click",()=>{
+  window.open("speech.html","_self");
+ 
+});
+document.getElementById("nop5").addEventListener("click",()=>{
+  window.open("QR.html","_self");
+ 
+});
+document.getElementById("nop3").addEventListener("click",()=>{
+  window.open("binary.html","_self")
+});
+function upd() {
+  let cdt = document.querySelector("#ctd").value;
+  let ou1 = document.getElementById("rp1");
+  let ou2 = document.getElementById("rp2");
+  let ou3 = document.getElementById("rp3");
+  let ou4 = document.getElementById("rp4");
+  let len = cdt.length;
+  ou1.innerHTML = len;
+  ou2.innerHTML = cdt.split(" ").length;
+  ou3.innerHTML = cdt.split(" ").length * 0.7 + " s";
+  if (cdt === "") {
+    ou4.innerHTML = "note null";  // empty case
+  }
+  else if (cdt === cdt.toUpperCase()) {
+    ou4.innerHTML = "UPPERCASE";  // all letters uppercase
+  }
+  else if (cdt === cdt.toLowerCase()) {
+    ou4.innerHTML = "lowercase";  // all letters lowercase
+  }
+  else {
+    ou4.innerHTML = "Mixed Case"; // अगर दोनों mix हैं
+  }
+}
+function up() {
+  let ctd = document.getElementById("ctd");
+  let cdt = document.querySelector("#ctd").value;
+  let ou4 = document.getElementById("rp4");
+  ctd.value = cdt.toUpperCase();
+  ou4.innerHTML = "UPPERCASE"; 
+}
+function low() {
+  let ctd = document.getElementById("ctd");
+  let cdt = document.querySelector("#ctd").value;
+  let ou4 = document.getElementById("rp4");
+  ctd.value = cdt.toLowerCase();
+  ou4.innerHTML = "lowercase"; 
+}
+function cltxt() {
+  let ou1 = document.getElementById("rp1");
+  let ou2 = document.getElementById("rp2");
+  let ou3 = document.getElementById("rp3");
+  let ou4 = document.getElementById("rp4");
+  let ctd = document.getElementById("ctd");
+  ou1.innerHTML = "0.00";
+  ou2.innerHTML = "0.00";
+  ou3.innerHTML = "0.00 s";
+  ou4.innerHTML = "Not Applicable";
+  alert("Your text is Removed")
+  ctd.value = "";
+}
+function copyText() {
+  let input = document.getElementById("ctd");
+  input.select();
+  input.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied Text: " + input.value);
+}
+
+document.getElementById("nop2").addEventListener("click", () => {
+  textcont.style.display = "flex";
+  opn2.style.display = "none";
+});
+document.getElementById("smartAI").addEventListener("click", () => {
+  const spk = new SpeechSynthesisUtterance("Hello !,I am Listening");
+  spk.lang = "en-GB";
+  spk.volume = 1;
+  spk.rate = 1;
+  spk.pitch = 1;
+  window.speechSynthesis.speak(spk);
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  if (!SpeechRecognition) {
+    alert("Speech Recognition is not supported in this browser. Use Chrome or Edge.");
+  }
+  else {
+    const reco = new SpeechRecognition();
+    reco.lang = langSelect.value || 'en-GB';
+    reco.interimResults = false;
+    reco.pitch = 1;
+    reco.onresult = (event) => {
+      let transcript = event.results[0][0].transcript;
+      let text = transcript.toLowerCase().trim();
+      console.log("User said:", text);
+      // smart block start
+      if (text.trim() !== "") {
+        let url = "https://www.google.com/search?q=" + encodeURIComponent(text);
+        window.open(url, "_blank"); // new tab me open hoga
+      } else {
+        alert("Please type something to search!");
+      }
+      // smart block close 
+    }
+    setTimeout(() => {
+      reco.start();
+      console.log("Listening...");
+    }, 2000);
+  }
+  setTimeout(() => {
+    listning.style.display = "flex"
+    console.log("Listening...");
+  }, 2000);
+});
+document.getElementById("x").addEventListener("click", () => {
+  google.style.display = "none";
+
+})
+document.getElementById("xx").addEventListener("click", () => {
+  zoracard.style.display = "none";
+})
+document.getElementById("xxx").addEventListener("click", () => {
+  textcont.style.display = "none";
+})
+document.querySelector(".image2").addEventListener("click", () => {
+  google.style.display = "flex";
+})
+document.querySelector(".image1").addEventListener("click", () => {
+  zoracard.style.display = "flex";
+})
+document.getElementById("target1").addEventListener("click", () => {
+  if (opn1.style.display == "none") {
+    opn1.style.display = "flex";
+  }
+  else {
+    opn1.style.display = "none";
+  }
+});
+
+document.querySelector(".image3").addEventListener("click", () => {
+  if (opn2.style.display == "none") {
+    opn2.style.display = "flex";
+  }
+  else {
+    opn2.style.display = "none";
+  }
+});
+
+function searchNow() {
+  let query = document.getElementById("input1").value;
+  if (query.trim() !== "") {
+    let url = "https://www.google.com/search?q=" + encodeURIComponent(query);
+    window.open(url, "_blank"); // new tab me open hoga
+  } else {
+    alert("Please type something to search!");
+  }
+}
+
+// Enter key se bhi search ho
+document.getElementById("input1").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    searchNow();
+  }
+});
+
+
+
+// voice asssitant first with google
+document.getElementById("voive").addEventListener("click", () => {
+  const langSelect = document.getElementById('langSelect');
+  let hint = document.querySelector(".hint");
+  hint.style.display = "flex";
+  const spk = new SpeechSynthesisUtterance("Hello! dear User, I am Zora AI, How can I help you today");
+  spk.lang = langSelect.value || 'ur-PK';
+  spk.volume = 1;
+  spk.rate = 1;
+  spk.pitch = 1;
+  window.speechSynthesis.speak(spk);
+
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  if (!SpeechRecognition) {
+    alert("Speech Recognition is not supported in this browser. Use Chrome or Edge.");
+  }
+  else {
+    const reco = new SpeechRecognition();
+    reco.lang = langSelect.value || 'en-GB';
+    reco.interimResults = false;
+    reco.pitch = 1;
+
+    reco.onresult = (event) => {
+      let transcript = event.results[0][0].transcript;
+      let text = transcript.toLowerCase().trim();
+      console.log("User said:", text);
+
+      if (text.includes("open youtube") || text.includes("youtube kholo") || text.includes("youtube khol do") || text.includes("youtube khol") || text.includes("youtube khol de")) {
+        window.open("https://www.youtube.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening Youtube");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open google") || text.includes("google kholo") || text.includes("google khol do") || text.includes("google khol") || text.includes("google khol de")) {
+        window.open("https://www.google.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening Google");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open facebook") || text.includes("facebook kholo") || text.includes("facebook khol do") || text.includes("facebook khol") || text.includes("facebook khol de")) {
+        window.open("https://www.facebook.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening Facebook");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open instagram") || text.includes("instagram kholo") || text.includes("instagram khol do") || text.includes("instagram khol") || text.includes("instagram khol de")) {
+        window.open("https://www.instagram.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening instagram");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open amazon") || text.includes("amazon kholo") || text.includes("amazon khol do") || text.includes("amazon khol") || text.includes("amazon khol de")) {
+        window.open("https://www.amazon.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening amazon");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open flipkart") || text.includes("flipkart kholo") || text.includes("flipkart khol do") || text.includes("flipkart khol") || text.includes("flipkart khol de")) {
+        window.open("https://www.flipkart.com", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening flipkart");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+
+      else if (text.includes("open sarkari result") || text.includes("sarkari result kholo") || text.includes("sarkari result khol do") || text.includes("sarkari result khol") || text.includes("sarkari result khol de")) {
+        window.open("https://sarkariresult.com.cm/", "_blank");
+        window.open("https://sarkariresult.com.im/#google_vignette", "_blank");
+        window.open("https://sarkariresultinfos.com/", "_blank");
+        const spk = new SpeechSynthesisUtterance("Opening sarkari result");
+        spk.lang = langSelect.value || 'en-GB';
+        spk.volume = 1;
+        spk.rate = 1;
+        spk.pitch = 1;
+        window.speechSynthesis.speak(spk);
+      }
+    }
+
+    setTimeout(() => {
+      reco.start();
+      console.log("Listening...");
+    }, 5500);
+  }
+});
+document.getElementById("idp1").addEventListener("click",()=>{
+    alert("Error 404 : AI not Available Yet")
+    document.body.innerHTML = " ";
+  document.body.style.background= "white";
+});
+document.getElementById("idp2").addEventListener("click",()=>{
+    alert("Error 404 : Page not Found")
+    document.body.innerHTML = " ";
+  document.body.style.background= "white";
+});
+document.getElementById("idp3").addEventListener("click",()=>{
+    alert("Error 404 : Not Available")
+    document.body.innerHTML = " ";
+  document.body.style.background= "white";
+});
+document.getElementById("idp4").addEventListener("click",()=>{
+window.open("https://www.youtube.com","_blank")
+});
+
+ddmmoo = () =>{
+  alert("Error 404: this page currently Unavailable");
+  document.body.innerHTML = " ";
+  document.body.style.background= "white";
+}
+
+
+
+if (window.innerWidth < 720) {
+  // alert("mobile");
+}
+else {
+  // alert("desktop")
+}
